32607011d2fb6013728ab130a563b1952e947d37
Ankit Raj <ankitraj6241@gmail.com>
2023-05-02 05:59:43 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const http = require('http');
+http.createServer(function(req,res){
+	res.write('on the way to fullstack engineer ');
+	res.end();
+}).listen(3000);
+console.log('server started on port 3000');
